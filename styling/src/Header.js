3b054aaaf0103ce993797ea2e4c2cc9a96c1aaea
nitroEef <mikebodetelli@gmail.com>
2024-03-19 10:18:12 +0100

    yudpodj

--- styling/src/Header.js
+++ styling/src/Header.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 const header = () => {
-  return (
-    <header style={{background:"blue", color:'red'}}>
+  const headerStyle = {
+    background:"green"}
+  
+  
+return (
+    <header>
       <h1>Cohort 4.0 students detail</h1>
     </header>
-  )
+  );
 }
 
 export default header
+
+// direct inline styling 
+// {/* <header style={{background:"blue", color:'red'}}>
+//       <h1>Cohort 4.0 students detail</h1>
+//     </header> */}
+
+// using variable inline styling 
+
+// const header = () => {
+//   const headerStyle = {
+//     background:"green"}
+  
+  
+{/* <header style={headerStyle}>
+<h1>Cohort 4.0 students detail</h1>
+</header> */}
