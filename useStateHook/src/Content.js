8ae6d20ff4bfe3c85994e7b640c3d4a8848a4ece
nitroEef <mikebodetelli@gmail.com>
2024-03-20 12:36:13 +0100

    update

--- useStateHook/src/Content.js
+++ useStateHook/src/Content.js
@@ -1,63 +1,78 @@
-// rafce 
-import { useState } from 'react';
-import { GoTrash } from "react-icons/go";
+// local storage is an object in js , not a method or function 
+// but it is a part of web storage api and provide method for 
+// storing and accessing data locally in the users web browsers 
+// it cant work alone without JSON.stringify and JSON.parse 
 
-import './content.css'
+
+import React, { useState } from "react";
+import { FaTrashRestoreAlt } from "react-icons/fa";
+import "./content.css";
 
 const Content = () => {
-  // const [name, setName] = useState('basirah') 
+  // const [name, setName] = useState('Basirah')
   const [items, setItems] = useState([
     {
-      id : 1,
-      checked : false,
-      item : 'garri'
-
+      id: 1,
+      checked: false,
+      item: "A bag of Garri",
     },
     {
-      id : 2,
-      checked : false,
-      item : 'bread'
-
+      id: 2,
+      checked: false,
+      item: " Rice",
     },
-
     {
-      id :3,
-      checked : false,
-      item : 'chips'
-
+      id: 3,
+      checked: false,
+      item: "Semo",
     },
-
     {
-      id : 4,
-      checked : false,
-      item : 'beans'
-
+      id: 4,
+      checked: false,
+      item: "Ponmo",
     },
-  ])
+  ]);
 
+  const handleCheck = (id) => {
+    // console.log(key: ${id});
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
 
-const handleCheck = (id) => {
-  // console.log(`key:${id}`)
-} 
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+      const listItems = items.filter((item) => item.id !== id); 
+      setItems(listItems);
+  }
 
   return (
-    <main className='student'>
-<ul>
-  {items.map((item) => (
-   <li className='item' key={item.id}>
-    <input type="checkbox" 
-    onClick={()=>handleCheck(item.id)}
-    checked={item.checked} />
-    <label>{item.item}</label>
-    <GoTrash />
-    <button>Delete</button>
+    <main className="student">
+      <ul>
+        {items.map((item) => {
+          return (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onClick={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
 
-
-</li> 
-  ))}
-</ul> 
+              <label 
+              style={(item.checked) ? {textDecoration:"line-through"} :null}
+              onDoubleClick={() => handleDelete(item.id)} >{item.item}</label>
+              <FaTrashRestoreAlt
+             // onClick={() => handleDelete(item.id)} //
+              />
+              
+            </li>
+          );
+        })}
+      </ul>
     </main>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
