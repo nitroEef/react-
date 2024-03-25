f74410b7313e4ce62f28fcf1cd569c9db484ca98
nitroEef <mikebodetelli@gmail.com>
2024-03-25 11:55:47 +0100

    update

--- propsAndPropDrilling/src/ItemList.js
+++ propsAndPropDrilling/src/ItemList.js
@@ -8,25 +8,7 @@ const Itemlist = ({items, handleCheck, handleDelete}) => {
     <ul>
           {items.map((item) => {
             return (
-              <li className="item" key={item.id}>
-                <input
-                  type="checkbox"
-                  onClick={() => handleCheck(item.id)}
-                  checked={item.checked}
-                />
-  
-                <label
-                  style={(item.checked) ? {textDecoration: "line-through"} : null}
-                  onDoubleClick={() => handleDelete(item.id)}
-                >
-                  {item.item}
-                </label>
-                <FaTrashRestoreAlt
-                onClick={() => handleDelete(item.id)} 
-                role="button"
-                tabIndex="0"
-                />
-              </li>
+              
             );
           })}
         </ul>
--- propsAndPropDrilling/src/LineItem.js
+++ propsAndPropDrilling/src/LineItem.js
@@ -1,10 +1,26 @@
 
 const LineItem = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+        return (
+        <li className="item" key={item.id}>
+        <input
+            type="checkbox"
+            onClick={() => handleCheck(item.id)}
+            checked={item.checked}
+        />
+
+        <label
+        style={(item.checked) ? {textDecoration: "line-through"} : null}
+        onDoubleClick={() => handleDelete(item.id)}
+        >
+            {item.item}
+        </label>
+        <FaTrashRestoreAlt
+        onClick={() => handleDelete(item.id)} 
+        role="button"
+        tabIndex="0"
+        />
+        </li>
+        )
+        }
 
 export default LineItem
