8591eda309f3e58472cef98fd06e7048ec1d8f37
nitroEef <mikebodetelli@gmail.com>
2024-04-03 10:15:45 +0100

    utir

--- statechallenge/src/List.js
+++ statechallenge/src/List.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ListItem from './ListItem'
 
-const List = () => {
+const List = ({items}) => {
   return (
     <ul>
-      
+      {items.map((item) => (
+        <ListItem key={item.id} item={item} />
+      ))}
     </ul>
   )
 }
